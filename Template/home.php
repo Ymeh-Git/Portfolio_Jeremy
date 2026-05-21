5b7b6b5b5f9c4a2e5c15b738fc618c4992f32e42
Jeremy_Droulez <jeremy.droulez.pro@gmail.com>
2026-05-21 09:53:45 +0200

    Homepage done
    Depending if $page == "home"
      Add video
      Add background over video

--- Template/home.php
+++ Template/home.php
@@ -4,4 +4,9 @@ if (!defined('ACCESS_GRANTED')) {
     exit();
 }
 ?>
-<h1>Home</h1>
+<section class="home-section">
+    <h1>Hi, I am Jérémy</h1>
+    <h2>This is my Portfolio</h2>
+    <p>It has been built with PhP, HTML & CSS.</p>
+    <p>I hope you enjoy it.</p>
+</section>
