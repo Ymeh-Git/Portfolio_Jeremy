3c53e32503d8de1cc824bdceb278ebc5fe442e0b
Jeremy_Droulez <jeremy.droulez.pro@gmail.com>
2026-05-22 09:50:28 +0200

    Contact page

--- Template/contact.php
+++ Template/contact.php
@@ -5,5 +5,14 @@ if (!defined('ACCESS_GRANTED')) {
 }
 ?>
 <section class="contact-section">
-    <h1>Contact</h1>
-</section>
+    <h1>Contact me</h1>
+    <a href="https://www.linkedin.com/in/jrmy-drlz" target="_blank" class="contact-card">
+        <img src="https://skillicons.dev/icons?i=linkedin" alt="LinkedIn logo">
+        <img src="./Assets/Images/linkedInQRCode.png" alt="QR code for LinkedIn profile">
+        <p>LinkedIn - Jeremy Droulez</p>
+    </a>
+    <a href="https://github.com/Ymeh-Git" target="_blank" class="contact-card">
+        <img src="./Assets/Images/sllmnhx-github-pixabay.svg" alt="GitHub logo">
+        <p>GitHub - Ymeh-Git<br>(Jeremy_Droulez)</p>
+    </a>
+    </section>
